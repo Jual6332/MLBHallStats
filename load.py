9b770d59ba2264dd6b7cf9a6f1dfc37bb0f94234
Jual6332 <jual6332@colorado.edu>
2017-01-21 00:02:02 -0700

    Displays Max Values to the user

--- load.py
+++ load.py
@@ -4,14 +4,17 @@ class Player:
 	pass
 
 # Construct Player Class
+val = 0
 CFs = []
+maxValues = [0] * 7
+maxNames = [0] * 7
 
 # Load Data for all CFs
 f = open("cfs.txt")
 lines = f.readlines()
 
 for ln in lines[1:len(lines)]:
-	plyr = Player()
+	plyr = Player() # Initialize
 	layer = ln.strip().split()
 	#print(type(player[1])) - Check type, is a string
 
@@ -25,10 +28,18 @@ for ln in lines[1:len(lines)]:
 	plyr.runs = layer[7]
 	plyr.sb   = layer[8] 
 
-	CFs.append(plyr)
+	CFs.append(plyr) # Add to array
 
-for num in CFs:
-	print num.name
+for player in CFs:
+	if player.avg > maxValues[0]:
+		maxValues[0] = player.avg
+		maxNames[0] = player.name
+	if player.obp > maxValues[1]:
+		maxValues[1] = player.obp
+		maxNames[1] = player.name
+
+print "Max Avg: " + maxValues[0] + " by " + maxNames[0]
+print "Max OBP: " + maxValues[1] + " by " + maxNames[1]
 
 f.close()
 
